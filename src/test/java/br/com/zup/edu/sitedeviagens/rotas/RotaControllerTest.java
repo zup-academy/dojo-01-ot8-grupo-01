c525cec2b540d8090c157e00ba9466ef84b21f9a
Wallyson <wallyson.silva@zup.com.br>
2021-09-03 17:23:10 -0300

    git commit -m "piloto {Wallyson} co-piloto {Alana}

--- src/test/java/br/com/zup/edu/sitedeviagens/rotas/RotaControllerTest.java
+++ src/test/java/br/com/zup/edu/sitedeviagens/rotas/RotaControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.zup.edu.sitedeviagens.rotas;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,12 +22,21 @@ public class RotaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    Gson gson = new Gson();
+
     @Test
-    public void testaAeroportosIguais() throws Exception {
-
-
+    public void quandoBodyNaoEnviadoRetornaBadRequest() throws Exception {
         mockMvc.perform(post("/rotas").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void quandoBodyValidoEnviadoRetornaOk() throws Exception {
+        RotaRequest rotaRequest = Mockito.mock(RotaRequest.class);
+        mockMvc.perform(post("/rotas").accept(MediaType.APPLICATION_JSON)
+                .content()
+                .contentType(MediaType.APPLICATION_JSON)
+                .andExpect(status().isOk());
     }
 
 }
